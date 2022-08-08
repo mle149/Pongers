fd09391c8227bd5fafff227c992be0404b7da9c9
mle149 <leemi546@gmail.com>
2022-08-08 21:21:32 +1200

    Added Enums (GameState & Player), Classes(Sprite, Ball, Paddle)

--- Pongers/src/Panel.java
+++ Pongers/src/Panel.java
@@ -10,6 +10,13 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 
 public class Panel extends JPanel implements ActionListener,KeyListener{
+	//variables
+	private static final int POINTS_TO_WIN = 3;
+	private static final int BALL_SPEED = 3;
+	int Player1Score = 0;
+	int Player2Score = 0;
+	
+	
 	
 	// Panel constructor
 	public Panel() {
@@ -20,7 +27,14 @@ public class Panel extends JPanel implements ActionListener,KeyListener{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);		
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setStroke(new BasicStroke(10));
+		//g2d.setStroke(new BasicStroke(10));
+		BasicStroke bs = new BasicStroke(5.0f, // Width 
+							BasicStroke.CAP_SQUARE, // End cap 
+							BasicStroke.JOIN_MITER, // Join style 
+							10.0f, // Miter limit 
+							new float[] {16.0f,20.0f}, // Dash pattern 
+							0.0f); //
+		g2d.setStroke(bs);
 		g2d.drawLine( (getWidth()/2), 0, (getWidth()/2), getHeight() );
 		g2d.setColor(Color.WHITE);
 		
